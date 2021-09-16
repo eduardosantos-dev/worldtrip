42a123920a1414ef8f745749e27450022090eb13
Eduardo Santos <eduardosantos.ist@gmail.com>
2021-09-16 18:53:47 -0300

    Estrutura inicial do projeto

---
+++ styles/theme.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      50: "#e7f5fc",
+      100: "#cfdddf",
+      200: "#b5c5c8",
+      300: "#9badb1",
+      400: "#809699",
+      500: "#677d80",
+      600: "#47585B",
+      700: "#364549",
+      800: "#1e2b2c",
+      900: "#001111",
+    },
+  },
+  fonts: {
+    headings: "Poppins",
+    body: "Poppins",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "white",
+        color: "gray.600",
+      },
+    },
+  },
+});

7d3266a5f90c104922c7555a9e405ab730bd6a1c
Eduardo Santos <eduardosantos.ist@gmail.com>
2021-09-17 18:08:41 -0300

    Adicionando cores ao tema

--- styles/theme.ts
+++ styles/theme.ts
@@ -15,6 +15,18 @@ export const theme = extendTheme({
       900: "#001111",
     },
     highlight: "#FFBA08",
+    highlight50: "rgba(255,186,8, 0.5)",
+    dark: {
+      black: "#000",
+      text: "#47585B",
+      info: "#999999",
+      info50: "rgba(153,153,153,0.5)",
+    },
+    light: {
+      white: "#FFFFFF",
+      text: "#F5F8FA",
+      info: "#DADADA",
+    },
   },
   fonts: {
     headings: "Poppins",
